de1b9672347f0e53628ea4457b6564704938592d
Fab <45894561+FabLong@users.noreply.github.com>
2022-04-03 17:30:05 +0100

    Update master_sender.ino

--- master_sender/master_sender/master_sender.ino
+++ master_sender/master_sender/master_sender.ino
@@ -12,21 +12,29 @@
 # define I2C_SLAVE3_ADDRESS 10 // Pretend slave 3 POC
 
 // Config variables
-String modesList[4] = {"Off  ", "Sleep" , "Away " , "Home "}; // Spaces in mode so LCD removes character when updating screen.
+String modesList[4] = {"Off  ", "Fab  " , "Solly" , "Dan  "}; // Spaces in mode so LCD removes character when updating screen.
 int numberModes = 4; // Length of the number of modes we have set
 int sleepLocations[1] = {1}; //ignore these rooms when we are sleeping, e.g {1,5} would ignore slave arduion's 1 and 5
 int numberSleepLocations = 1;
 
 // Create constants for Pin Names
 const int rs = 12, en = 11, d4 = 4, d5 = 5, d6 = 6, d7 = 7;
-//int const SpeakerPin = 8; pin 8 is now being used by LCD to set contrast.
+int const piezo = 10; //pin 8 is now being used by LCD to set contrast.
 int const ButtonOnePin = 2;
 int const ButtonTwoPin = 3;
 int const servoPin = 13;
-int const SpeakerPin =0; //need to change this 
+//int const SpeakerPin = 0; //need to change this 
 
 // NEW variables
 int Contrast = 60; // Sets LCD contrast value.
+int pressedBasic = 0; // Boolean to track whether the basic button has been pressed during interrupt.
+int pressedSecret = 0; // Boolean to track whether the secret button has been presssed during interrupt.
+int servoPosition = 90;
+
+int requestSlave1 = 0; // Boolean to track whether to Slave 1 needs to make a request
+int requestSlave2 = 0; // Boolean to track whether to Slave 1 needs to make a request
+int requestDoorbell = 0; // Boolean to track whether to call doorbell ringtone code.
+
 
 //setup libarys
 SoftwareSerial SoftSerial(10, 9);
@@ -37,6 +45,7 @@ Servo modeServo;
 //definining global variables
 char message[7];
 int mode = 0;
+int lastMode = mode;
 
 //creating pixart
 byte distance[8] {
@@ -86,7 +95,7 @@ String allowsValues[] = {"3B00D56D9516", "310098A80100"};
 int numberAllowed = 2; // Length of the number of allowed values we have in our list
 int authenticated = 0; // Set authenticated value to 0, indicating unauthenticated status.
 // Screen dims in 'Off' mode.
-int servoLocation[] = {0, 60, 120, 180};
+int servoLocation[] = {90, 0, 0, 180};
 
 void setup() {
   Wire.begin();        // Join i2c bus (address optional for master)
@@ -100,48 +109,79 @@ void setup() {
   lcd.createChar(2, light);
   lcd.createChar(3, modePixel);
 
-  Serial.begin(9600);  // Start serial for output // do we want this.
-  SoftSerial.begin(9600);     // RFID (the SoftSerial baud rate)
-  attachInterrupt(0, setMode, FALLING); // Attaching interrupt so button changes the mode of system.
-  attachInterrupt(1, setMode, FALLING);
+  //Serial.begin(9600);  // Start serial for output // do we want this.
+  //SoftSerial.begin(9600);     // RFID (the SoftSerial baud rate)
+  attachInterrupt(1, doorbell, FALLING); // Attaching interrupt so button changes the mode of system.
+  attachInterrupt(0, setMode, FALLING);
   pinMode(ButtonOnePin, INPUT_PULLUP);
   pinMode(ButtonTwoPin, INPUT_PULLUP);
   modeServo.attach(servoPin);
 
   // Set Initial mode on LCD and Servo position.
+  mode = 0;
   String modeName = modesList[mode];
   lcd.setCursor(10, 1);
   lcd.write(byte(3));
   lcd.print(modeName);
-  setServo();
+  modeServo.write(servoPosition); // set servo initial position.
 }
 
 void loop() {
-  int lastMode = mode;
-  int potValue = analogRead(A0); // Poteniometer value
-  potValue = map(potValue, 0, 1023, 0, 255); // Map the analogue value between 0 and 255, change this.
+  String modeName = modesList[mode];
+  lcd.setCursor(10, 1);
+  lcd.write(byte(3));
+  lcd.print(modeName);
   
-  getMessage(I2C_SLAVE1_ADDRESS); // Gets the message from slave 1
-  checkMessage(I2C_SLAVE1_ADDRESS); // Shows the message on our LCD and handles it if nesssecary
-  checkModeChange(lastMode);
+  
+  int potValue = analogRead(A0); // Poteniometer value
+  potValue = map(potValue, 0, 1023, 0, 3); // Map the analogue value: modes 0, 1, 2, 3
+  modeName = modesList[potValue];
+  lcd.setCursor(0, 1);
+  lcd.print("SELECT:   ");
+  lcd.write(byte(3));
+  //lcd.print(" ");
+  lcd.print(modeName);
+
+  
   lastMode = mode;
 
-  delay(200);
-  getMessage(I2C_SLAVE2_ADDRESS); // Gets the message from slave 2
-  checkMessage(I2C_SLAVE2_ADDRESS); // Shows the message on our LCD and handles it if nesssecary
-  checkModeChange(lastMode);
+
+
+  if(requestDoorbell == 1){
+    doorbell_sound(); //CHANGE ME TO NICE SOUND
+    requestDoorbell = 0;
+  }
+  checkModeChange();
+  lastMode = mode;
+
+ 
+  checkModeChange();
   lastMode = mode;
   delay(200);
   // This code is in the loop; can't run lcd code in interrupt.
-  checkModeChange(lastMode);
+  checkModeChange();
   lastMode = mode;
   
+  if(servoPosition != 90){
+    delay(1000);
+    servoPosition = 90;
+    modeServo.write(90); // reset servo position.
+  }
+
+  lcd.setCursor(0,0);
+  lcd.print("               ");
 }
 
-void checkModeChange(int lastMode){
+void checkModeChange(){
   if (mode != lastMode) { // If our mode has changed within the loop we update this on the LCD
     String modeName = modesList[mode];
-
+    int adress;
+    if (mode == 1){
+      adress = I2C_SLAVE1_ADDRESS;
+    }
+    else{
+      adress = I2C_SLAVE2_ADDRESS;
+    }
     // If mode is 'off' we want to clear the data on top row of LCD.
     if (mode == 0){
       lcd.setCursor(0,0);
@@ -150,6 +190,25 @@ void checkModeChange(int lastMode){
     lcd.setCursor(10, 1);
     lcd.write(byte(3));
     lcd.print(modeName);
+    //Serial.write("11");
+    getMessage(adress); // Gets the message from slave 2
+    checkMessage(adress); // Shows the message on our LCD and handles it if nesssecary
+    delay(1000);
+    getMessage(adress); // Gets the message from slave 2
+    checkMessage(adress);
+
+    if(mode == 3){
+      lcd.setCursor(0, 0);
+      lcd.write(byte(3));
+      lcd.print("Really?");
+    } else{
+      lcd.setCursor(0, 0);
+      lcd.write(byte(3));
+      lcd.print("Sexy ;)");
+    }
+    
+    mode = 0;
+    lastMode = 0;
   }
 }
 /*
@@ -157,137 +216,26 @@ void checkModeChange(int lastMode){
    Parameters: slaveAdress
 */
 void checkMessage(int slaveAdress) {
-  // ADD more functions to deal with data.
-
-  // Store data as ints for computation.
-  // Converts chars to int.
-  int hundreds = message[3]-'0';
-  hundreds = hundreds * 100;
-  int tens = message[4]-'0';
-  tens = tens * 10;
-  int digits = message[5]-'0';
-  int data = hundreds + tens + digits;
-
   String toPrint = "" ;
   char type;
   char sensor;
   int skip = 0;
-  type = message [0];
-  if (type == 'S') { //if our message is from a sensor
-    if (mode == 1) { //if our mode is sleep then we want to ignore messages from certain slaves 
-        for (int x = 0; x < numberSleepLocations; x ++) {
-          if (message[2] == sleepLocations[x]) {//checking whether this slave is in the locations to ignore
-            skip = 1;
-          }
-        }
-      }
-    if (!(mode == 0 or skip ==1) ) { //if not the mode is 0, or skip ==1. If either/both of these are true, then our if statement will return false, and not run the below code
-      
-      sensor = message[1];
-      switch (sensor) {
-        case 'U': //ultrasound
-
-          // Checks if Ultrasound has been interfered with by an object within 100cm.
-          if (data < 100){
-            toPrint.concat("CLOSE");
-            toPrint.concat(':');
-            toPrint.concat(message[2]);
-            lcd.setCursor(0, 1);
-            lcd.print(toPrint);
-            lcd.setCursor(0, 1); //reset our cursor location
-            toPrint = "";
-          }
-          else {
-            lcd.setCursor(0, 1); //reset our cursor location
-            lcd.write("        "); //clear our display when distance greater than 100cm.
-          }
-
-          toPrint.concat(message[3]); // This adds our data to be printed
-          toPrint.concat(message[4]);
-          toPrint.concat(message[5]); 
-          toPrint.concat(' ');
-          lcd.setCursor(0, 0);
-          lcd.write(byte(0));
-          lcd.print(toPrint);
-          break;
-          
-        case 'T': //thermistor
-          // Checks if temperature change greater than 5 degree celcius.
-          if (data > 5){
-            toPrint.concat("TEMP");
-            toPrint.concat(':');
-            toPrint.concat(message[2]);
-            lcd.setCursor(0, 1);
-            lcd.print(toPrint);
-            setAlarm(69); // 69 changes the tone.
-            lcd.setCursor(0, 1); //reset our cursor location
-            toPrint = "";
-            delay(200);
-          }
-          else {
-            lcd.setCursor(0, 1); //reset our cursor location
-            lcd.write("        "); //clear our display when temperature change less than 5 degrees celcius.
-          }
-          toPrint.concat(message[3]); // This adds our data to be printed
-          toPrint.concat(message[4]);
-          toPrint.concat(message[5]);
-          toPrint.concat(' ');
-          lcd.setCursor(5, 0);
-          lcd.write(byte(1));
-          lcd.print(toPrint);
-          break;
-          
-        case 'L': //photo resistor
-          if (data < 100){
-            toPrint.concat("DARK");
-            toPrint.concat(':');
-            toPrint.concat(message[2]);
-            lcd.setCursor(0, 1);
-            lcd.print(toPrint);
-            lcd.setCursor(0, 1); //reset our cursor location
-            toPrint = "";
-          }
-          else if (data > 800) {
-            toPrint.concat("BRIGHT");
-            toPrint.concat(':');
-            toPrint.concat(message[2]);
-            lcd.setCursor(0, 1);
-            lcd.print(toPrint);
-            lcd.setCursor(0, 1); //reset our cursor location
-            toPrint = "";
-          }
-          else {
-            lcd.setCursor(0, 1); //reset our cursor location
-            lcd.write("        "); //clear our display when temperature change less than 5 degrees celcius.
-          }
-
-        
-          toPrint.concat(message[3]); // This adds our data to be printed
-          toPrint.concat(message[4]);
-          toPrint.concat(message[5]);
-          toPrint.concat(' ');
-
-          lcd.setCursor(10, 0);
-          lcd.write(byte(2));
-          lcd.print(toPrint);
-          break;
-      }
-    }
-  }
-  if (type == 'I' and mode != 0) { //if we have an input, and our mode is not off
+  type = message[0];
+  //Serial.print(type);
+  //Serial.write("1");
+  //Serial.write(type);
+  if (1) { //if we have an input, and our mode is not off
+    //Serial.write("3");
     sensor = message[1];
-    switch (sensor) {
-      case 'P': //if our message is the panic alarm.
         toPrint.concat("PANIC");
         toPrint.concat(':');
         toPrint.concat(message[2]);
         lcd.setCursor(0, 1);
         lcd.print(toPrint);
-        setAlarm(slaveAdress); 
+        //setAlarm(slaveAdress); 
         lcd.setCursor(0, 1); //reset our cursor location
         lcd.write("        "); //clear our display once the panic alarm has stopped running 
     }
-  }
 }
 /*
    Requests a mesasge from slave at adress specified in the paramater and adds it to our message variable
@@ -304,33 +252,20 @@ void getMessage(int slaveAdress) {
   }
   message[counter] = 0;
 }
-/*
-   This function will run our panic alarm, with frequency and duration between notes set by the type of alarm
-   Argumnets, type (integer)
-*/
-void setAlarm(int type) {
-  detachInterrupt(0); //using software serial interferes with interupt procces, so we detach the interupt while running our function
-  int firstNote;
-  int secondNote;
-  int duration;
-  firstNote = 800 + 20 * type; //changes the pitch of our alarms based on what has caused it
-  secondNote = 700 + 20 * type;
-  duration = 400;
 
-  for (int x = 0; x < 20; x++) {
-    /*
-     * Commmented out RFID checking code
-    if (checkRfid() == 1) {
-      break;
-    }
-    */
-    tone(SpeakerPin, firstNote, duration);
+void doorbell_sound(){
+  detachInterrupt(0); //using software serial interferes with interupt procces, so we detach the interupt while running our function
+  detachInterrupt(1);
+  for (int x = 0; x < 10; x++){
+    tone(piezo, 500, 400);
     delay(100);
-    tone(SpeakerPin, secondNote, duration);
+    tone(piezo,200, 400);
     delay(100);
   }
- attachInterrupt(0, setMode, FALLING); //reattaching intterupt for the rest of the program
+  attachInterrupt(1, doorbell, FALLING); //reattaching intterupt for the rest of the program
+  attachInterrupt(0, setMode, FALLING);
 }
+
 
 /*
    This function will run to check if we have a recognised rfid tag being sensed, and returns 1 if this is true, and 0 if false
@@ -367,19 +302,32 @@ int checkRfid() { //checks whether an rfid tag is valid
    Interrupt when we we press button, allows us to cycle through modes and will run the setServo() method to reflect this
 */
 void setMode() {
+  int potValue = analogRead(A0); // Poteniometer value
+  potValue = map(potValue, 0, 1023, 0, 3); // Map the analogue value: modes 0, 1, 2, 3
+  // We need to display changing of the modes, although we cant do this in the interrupt.
+  mode = potValue;
+
+  /*
+   * Old code
   if (mode == (numberModes - 1)) { //this means we are trying to add one to a full list (-1 becuase zero indexed), so we resest mode to the start
     mode = 0;
   }
   else {
     mode ++;
   }
+  */
   setServo();
   // Serial.print(mode);
+}
+
+void doorbell() {
+  requestDoorbell = 1;
 }
 
 /*
    Sets the servo to show the current mode
 */
 void setServo() {
+  servoPosition = servoLocation[mode];
   modeServo.write(servoLocation[mode]);
 }
